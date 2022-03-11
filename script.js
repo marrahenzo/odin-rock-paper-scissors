fbb708fb117a1bd7d5374136267d58cd5d80c361
Hernán Marrapodi <hernanmarra2002@gmail.com>
2022-03-11 00:03:21 -0300

    Removed unnecesary comments

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function computerPlay() {
   //randomly return rock paper or scissors
-  //get a random number between 1 and 3
   let play = Math.floor(Math.random() * 3) + 1;
-  //if number is 1, return rock, if 2, paper, if 3, scissors
   switch (play) {
     case 1:
       return "rock";
